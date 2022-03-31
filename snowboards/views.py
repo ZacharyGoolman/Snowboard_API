119d582ad38f9059572f71382db42f80345db526
ZacharyGoolman <zachary.goolman@gmail.com>
2022-03-31 17:52:10 -0400

    Registered Snowboard model with admin site + Completed post functionality for snowboard + Updated Rest Framework Decimal settings

--- snowboards/views.py
+++ snowboards/views.py
@@ -1,8 +1,21 @@
-import imp
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from .serializers import SnowboardSerializer
+from .models import Snowboard
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def snowboards_list(request):
 
-    return Response('ok')
+
+    if request.method == 'GET':
+        snowboards = Snowboard.objects.all()
+        serializer = SnowboardSerializer(snowboards, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = SnowboardSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+       
