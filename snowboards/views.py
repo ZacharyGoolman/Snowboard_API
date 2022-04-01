af6fc15d0772ee900234b12fb71975898605ccb1
ZacharyGoolman <zachary.goolman@gmail.com>
2022-04-01 12:04:16 -0400

    Created functionality to update Snowboard in snowboard app view file

--- snowboards/views.py
+++ snowboards/views.py
@@ -1,11 +1,9 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-
-import snowboards
 from .serializers import SnowboardSerializer
 from .models import Snowboard
-from snowboards import serializers
 
 @api_view(['GET', 'POST'])
 def snowboards_list(request):
@@ -22,14 +20,17 @@ def snowboards_list(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-@api_view(['GET'])
+@api_view(['GET', 'PUT'])
 def snowboard_detail(request, pk):
-    try:
-        snowboard = Snowboard.objects.get(pk=pk)
+    snowboard = get_object_or_404(Snowboard, pk=pk)
+    if request.method == 'GET':
         serializer = SnowboardSerializer(snowboard);
         return Response(serializer.data)
-
-    except Snowboard.DoesNotExist: 
-        return Response(status=status.HTTP_404_NOT_FOUND);  
-    
+    elif request.method == 'PUT':
+        
+        serializer = SnowboardSerializer(snowboard, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+       
    
